a96f2291d0229408d10852f5b8e07e2cc4e9362a
blosloos <mloos@envibee.ch>
2022-08-25 19:51:46 +0200

    Update multiform.R

--- R/multiform.R
+++ R/multiform.R
@@ -3,56 +3,44 @@ function(formula1,fact){
 
   formulas<-c();
   for(i in 1:length(formula1)){
+  
     ##############################################################################
-    # on first chemical formula ##################################################
-    formula1[i]<-gsub("D","[2]H",formula1[i]);
-    ende1<-nchar(formula1[i]);
-    element1<-c();
-    number1<-c();
+    # on first chemical formula
+    formula1[i] <- gsub("D", "[2]H", formula1[i])
+    ende1 <- nchar(formula1[i])
+    element1 <- number1 <- c()
     ##############################################################################
     # on formula1 (deduct)
-    j<-c(1);
-    while(j<=ende1){
-      if(substr(formula1[i],j,j)==c("[")){
-            b<-j
-            while(any(substr(formula1[i],j,j)==c("]"))!=TRUE){
-                j<-c(j+1);
-            };
-            k<-j
-            while(any(substr(formula1[i],j,j)==c("0","1","2","3","4","5","6","7","8","9"))!=TRUE){
-                j<-c(j+1);
-            };
-            m<-c(j-1);
-            element1<-c(element1,substr(formula1[i],b,m));
+    j <- 1
+    while( j <= ende1){
+      if(substr(formula1[i], j, j) == "["){
+            b <- j
+            while(!any(substr(formula1[i], j, j) == "]")) j <- j + 1
+            k <- j
+            while(!any(substr(formula1[i], j, j) == c("0","1","2","3","4","5","6","7","8","9"))) j <- j + 1
+            m <- j - 1
+            element1 <- c(element1,substr(formula1[i], b, m))
       }
-      if(any(substr(formula1[i],j,j)==c("0","1","2","3","4","5","6","7","8","9"))!=TRUE){
-            k<-j;
-            while(any(substr(formula1[i],j,j)==c("0","1","2","3","4","5","6","7","8","9"))!=TRUE){
-              j<-c(j+1);
-            };
-            m<-c(j-1);
-            j<-c(j-1);
-            element1<-c(element1,substr(formula1[i],k,m));
-      };
-      if(any(substr(formula1[i],j,j)==c("0","1","2","3","4","5","6","7","8","9"))==TRUE){
-            k<-j;
-            while(any(substr(formula1[i],j,j)==c("0","1","2","3","4","5","6","7","8","9"))==TRUE){
-              j<-c(j+1);
-            }
-            m<-c(j-1);
-            j<-c(j-1);
-            number1<-c(number1,as.numeric(substr(formula1[i],k,m)));
-      };
-    j<-j+1;
-    } # end while loop
-    ##############################################################################
-    # multiply ! #################################################################
-    number1<-fact*number1
-    formula_fin<-""
-    for(i in 1:length(element1)){
-      formula_fin<-paste(formula_fin,element1[i],number1[i],sep="")
+      if(!any(substr(formula1[i], j, j) == c("0","1","2","3","4","5","6","7","8","9"))){
+            k <- j
+            while(!any(substr(formula1[i], j, j) == c("0","1","2","3","4","5","6","7","8","9"))) j <- j + 1
+            m <- j <- j - 1
+            element1 <- c(element1, substr(formula1[i], k, m))
+      }
+      if(any(substr(formula1[i],j,j) == c("0","1","2","3","4","5","6","7","8","9"))){
+            k <- j
+            while(any(substr(formula1[i], j, j) == c("0","1","2","3","4","5","6","7","8","9"))) j <- j + 1
+            m <- j <- j - 1
+            number1 <- c(number1, as.numeric(substr(formula1[i], k, m)))
+      }
+    j <- j + 1
     }
-    formulas<-c(formulas,formula_fin);
+    ##############################################################################
+    # multiply
+    number1 <- fact * number1
+    formula_fin <- ""
+    for(i in 1:length(element1)) formula_fin <- paste0(formula_fin, element1[i], number1[i])
+    formulas <- c(formulas, formula_fin)
   }
   return(formulas)
   ##############################################################################
