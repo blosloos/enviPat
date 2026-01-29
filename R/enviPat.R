a52b5237fe3bdcc5ebfcc219fddb8cf1217f52da
blosloos <mloos@envibee.ch>
2026-01-29 15:30:13 +0100

    v 2.8 check, add Sciex7600, etc

--- R/enviPat.R
+++ R/enviPat.R
@@ -1,5 +1,9 @@
 .onAttach <- function(lib, pkg)
 {
-	packageStartupMessage(paste0("\n \n Welcome to enviPat version 2.5 \n Check https://www.envipat.eawag.ch for an interactive online version\n"));
+	packageStartupMessage(paste0("\n \n Welcome to enviPat version 2.8 
+	\n Check www.envipat.eawag.ch for an interactive online version
+	\n Check www.envimass.ch for a full workflow integration
+	\n"
+	));
 
 }
