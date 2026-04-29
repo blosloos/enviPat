ba3a86c851af7f004af8c4e0825d8580adeb826d
Martin Loos <mloos@envibee.ch>
2026-04-29 17:58:51 +0200

    packageStartupMessage version

--- R/enviPat.R
+++ R/enviPat.R
@@ -1,6 +1,6 @@
 .onAttach <- function(lib, pkg)
 {
-	packageStartupMessage(paste0("\n \n Welcome to enviPat version 2.8 
+	packageStartupMessage(paste0("\n \n Welcome to enviPat version 2.91 
 	\n Check www.envipat.eawag.ch for an interactive online version
 	\n Check www.envimass.ch for a full workflow integration
 	\n"
